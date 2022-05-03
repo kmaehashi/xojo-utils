4a524ebc4ae59afdf083439e83c701c45b22fa12
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2022-05-04 02:31:06 +0900

    fix import

--- xojo_tools/__init__.py
+++ xojo_tools/__init__.py
@@ -1,4 +1,4 @@
-from xojo_cli.communicator import IDECommunicatorV1
-from xojo_cli.communicator import IDECommunicatorV2
+from xojo_tools.communicator import IDECommunicatorV1
+from xojo_tools.communicator import IDECommunicatorV2
 
-from xojo_cli._version import __version__
+from xojo_tools._version import __version__
